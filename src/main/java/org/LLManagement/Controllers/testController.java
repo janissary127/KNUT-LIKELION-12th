92d2b531b6ca5f84b8cd00e78fe5094da509b15e
RACENI <saracen_dev@naver.com>
2024-03-26 01:08:06 +0900

    add loginController

--- src/main/java/org/LLManagement/Controllers/testController.java
+++ src/main/java/org/LLManagement/Controllers/testController.java
@@ -1,7 +1,6 @@
 package org.LLManagement.Controllers;
 
 
-import org.LLManagement.Models.AttendanceManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,10 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class testController {
     @GetMapping("hello")
     public String home(){
-        AttendanceManager llContent = new AttendanceManager();
-        //llContent.AttendanceChecking(123);
-        if(llContent.AttendanceChecked(123, 20240324))
-            System.out.println("gd");
-        return"hel2lo";
+        return"index";
     }
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -7,11 +7,11 @@
 </head>
 <body>
 <h2>회원가입</h2>
-<form action="/hello" method="post">
+<form action="/login2" method="post">
     <label for="username">사용자 이름:</label>
-    <input type="text" id="username" name="username" required><br>
+    <input type="text" id="username" name="userName" required><br>
     <label for="password">비밀번호:</label>
-    <input type="password" id="password" name="password" required><br>
+    <input type="password" id="password" name="userPassword" required><br>
     <button type="submit">가입</button>
 </form>
 </body>
